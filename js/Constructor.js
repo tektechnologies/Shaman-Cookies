64a8ae2bff2f5db31b4bd2836e71141231914065
tektechnologies <info@tektechnologies.com>
2018-06-18 19:44:03 -0500

    Add js and removes duplicate code and html mistakes

--- js/Constructor.js
+++ js/Constructor.js
@@ -1,146 +1,12 @@
-'use srict';
-
-//constructor for new store.
-function NewStore(minCustomers, maxCustomers, averageSales, locationName) {
-  this.minCustomers = minCustomers;
-  this.maxCustomers = maxCustomers;
-  this.averageSales = averageSales;
-  this.locationName = locationName;
-
-  // Add this new store to the catalog
-  NewStore.all.push(this);
-}
-
-// Add store to list of all stores.
-NewStore.all = [];
-
-NewStore.renderAll = function() {
-  // First, empty out the existing customer list
-  var storesUL = document.getElementById('allStores');
-  storesUL.innerHTML = '';
-
-  // Then, render each Store
-  for (var i = 0; i < NewStore.all.length; i++) {
-    NewStore.all[i].render();
-  }
-};
-
-//Prototype  to string that returns location name and average sales
-NewStore.prototype.toString = function () {
-  return this.locationName + ' (' + this.averageSales + ' Average Sales)';
-};
-//prototype to get allStores by element ID.
-NewStore.prototype.render = function() {
-  var allStoresUL = document.getElementById('allStores');
-
-  /* This is the same function as the one below.
-  var li = document.createElement('li');
-  li.textContent = this.toString();
-  customersUL.appendChild(li);
-  */
-  appendNewElement('li', this.toString(), allStoresUL);
-};
-
-function appendNewElement(tagName, textContent, parent) {
-  var el = document.createElement(tagName);
-  el.textContent = textContent;
-  parent.appendChild(el);
-}
-
-function handleSubmit(event) {
-  event.preventDefault();
-
-  console.log(event);
-
-  var minCustomers = event.target.minCustomers.value;
-  var maxCustomers = event.target.maxCustomers.value;
-  var averageSales = event.target.averageSales.value;
-  var locationName = event.target.locationName.value;
-
-
-
-  var newStore = new NewStore(minCustomers, maxCustomers, averageSales, locationName);
-  console.log(newStore);
-
-  NewStore.renderAll();
-}
-var form = document.querySelector('form');
-form.addEventListener('submit', handleSubmit);
-
-
-
-function ShamanCookieStores( minCustomers, maxCustomers, averageSales, locationName){
-  if (typeof this === 'undefined') { console.error('Cookie is a constructor! Use new!'); return; }
-  if (arguments.length < 4) { console.error('minCustomers, maxCustomers, averageSales, name are required!'); }
-
-  this.minCustomers = minCustomers;
-  this.maxCustomers = maxCustomers;
-  this.averageSales = averageSales;
-  this.locationName = locationName;
-  this.cookiesSold = [];
-}
-
-ShamanCookieStores.prototype.numOfCustPerHour = function() {
-  return Math.ceil(Math.random() * (this.maxCustomers - this.minCustomers) + this.minCustomers);
-};
-ShamanCookieStores.prototype.numOfCookiesPerHour = function() {
-  for(var i=0; i< hours.length; i++){
-    this.cookiesSold[i]= Math.floor(this.numOfCustPerHour() * this.averageSales);
-  }
-};
-ShamanCookieStores.prototype.toString = function() {
-  return this.name + '( ' + this.minCustomers + ' ' + this.maxCustomers + ' )';
-};
-
-var iowaCity = new ShamanCookieStores(23,65,6.3,'Iowa City');
-var cedarRapids = new ShamanCookieStores(3,24,1.2,'Cedar Rapids');
-var boulderColo = new ShamanCookieStores(11,38,3.7,'Boulder Colo.');
-var eugeneOregon = new ShamanCookieStores(20,38,2.3,'Eugene Oregon');
-var oxnardCali = new ShamanCookieStores(2, 16, 4.6,'Point Break');
+'use strict';
 var hours=['6am','7am','8am','9am','10am','11am','12pm','1pm','2pm','3pm','4pm','5pm','6pm','7pm','8pm'];
 var cookiesTotalHour = [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0];
 
-function displayForecastedLocationData(location){
-  // Run our cookies-per-hour simulation!
-  location.numOfCookiesPerHour();
-  // Let's also count total cookies
-  var cookieTotal = 0;
-  // Find parent <tbody> for this location by id
-  var tbody = document.getElementById('storeData');
-  var tr = document.createElement('tr');
-  tbody.appendChild(tr);
-  /////////////////////////////////////////////////
-  var tdLocationName = document.createElement('td');
-  tdLocationName.textContent = location.locationName;
-  tr.appendChild(tdLocationName);
-  //////////////////////////////////////////
-
-  // For each hour that the store is open...
-  for(var i = 0; i < hours.length; i++){
-    var cookiesForThisHour = location.cookiesSold[i];
-    cookiesTotalHour[i] += cookiesForThisHour;
-    cookieTotal += cookiesForThisHour;
-
-    var cookieHourlyData = document.createElement('td');
-    cookieHourlyData.textContent = cookiesForThisHour;
-    tr.appendChild(cookieHourlyData);
-  }
-  var cookieTotals = document.createElement('td');
-  cookieTotals.textContent = cookieTotal;
-  tr.appendChild(cookieTotals);
-
-
-}
-//create list items, create the objects identify html ul elements by id.
-displayForecastedLocationData(iowaCity);
-displayForecastedLocationData(cedarRapids);
-displayForecastedLocationData(boulderColo);
-displayForecastedLocationData(eugeneOregon);
-displayForecastedLocationData(oxnardCali);
 
 function footerHourlyStoreTotals(){
   var cookieTotal = 0;
   var tFoot = document.getElementById('tableFooter');
+  tFoot.innerHTML = '';
   var tr = document.createElement('tr');
   tFoot.appendChild(tr);
 
@@ -161,4 +27,104 @@ function footerHourlyStoreTotals(){
   cookieTotals.textContent = cookieTotal;
   tr.appendChild(cookieTotals);
 }
-footerHourlyStoreTotals();
+
+function ShamanCookieStores( minCustomers, maxCustomers, averageSales, locationName){
+  if (typeof this === 'undefined') { console.error('Cookie is a constructor! Use new!'); return; }
+  if (arguments.length < 4) { console.error('minCustomers, maxCustomers, averageSales, name are required!'); }
+
+  this.minCustomers = minCustomers;
+  this.maxCustomers = maxCustomers;
+  this.averageSales = averageSales;
+  this.locationName = locationName;
+  this.cookiesSold = [];
+
+  // Run our cookies-per-hour simulation!
+  this.numOfCookiesPerHour();
+
+  // Add this new store to the catalog
+  ShamanCookieStores.all.push(this);
+}
+
+// Add store to list of all stores.
+ShamanCookieStores.all = [];
+
+
+
+//this runs the number of cookies per hour
+ShamanCookieStores.prototype.numOfCookiesPerHour = function() {
+  for(var i=0; i< hours.length; i++){
+    this.cookiesSold[i]= Math.floor(this.numOfCustPerHour() * this.averageSales);
+  }
+};
+
+ShamanCookieStores.prototype.numOfCustPerHour = function() {
+  return Math.ceil(Math.random() * (this.maxCustomers - this.minCustomers) + this.minCustomers);
+};
+
+
+
+//Prototype  to string that returns location name and average sales
+ShamanCookieStores.prototype.render = function () {
+  // Let's also count total cookies
+  var cookieTotal = 0;
+  // Find parent <tbody> for this location by id
+  var tbody = document.getElementById('storeData');
+  var tr = document.createElement('tr');
+  tbody.appendChild(tr);
+  /////////////////////////////////////////////////
+  var tdLocationName = document.createElement('td');
+  tdLocationName.textContent = this.locationName;
+  tr.appendChild(tdLocationName);
+  //////////////////////////////////////////
+
+  // For each hour that the store is open...
+  for(var i = 0; i < hours.length; i++){
+    var cookiesForThisHour = this.cookiesSold[i];
+    cookiesTotalHour[i] += cookiesForThisHour;
+    cookieTotal += cookiesForThisHour;
+
+    var cookieHourlyData = document.createElement('td');
+    cookieHourlyData.textContent = cookiesForThisHour;
+    tr.appendChild(cookieHourlyData);
+  }
+  var cookieTotals = document.createElement('td');
+  cookieTotals.textContent = cookieTotal;
+  tr.appendChild(cookieTotals);
+};
+
+ShamanCookieStores.renderAll = function() {
+  // First, empty out the existing customer list
+  var tbody = document.getElementById('storeData');
+  tbody.innerHTML = '';
+  cookiesTotalHour = [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0];
+  // Then, render each Store
+  for (var i = 0; i < ShamanCookieStores.all.length; i++) {
+    ShamanCookieStores.all[i].render();
+  }
+  footerHourlyStoreTotals();
+};
+
+var iowaCity = new ShamanCookieStores(23,65,6.3,'Iowa City');
+var cedarRapids = new ShamanCookieStores(3,24,1.2,'Cedar Rapids');
+var boulderColo = new ShamanCookieStores(11,38,3.7,'Boulder Colo.');
+var eugeneOregon = new ShamanCookieStores(20,38,2.3,'Eugene Oregon');
+var oxnardCali = new ShamanCookieStores(2, 16, 4.6,'Point Break');
+
+
+//create list items, create the objects identify html ul elements by id.
+ShamanCookieStores.renderAll();
+
+function handleSubmit(event) {
+  event.preventDefault();
+  var minCustomers = event.target.minCustomers.value;
+  var maxCustomers = event.target.maxCustomers.value;
+  var averageSales = event.target.averageSales.value;
+  var locationName = event.target.locationName.value;
+
+  var newStore = new ShamanCookieStores(minCustomers, maxCustomers, averageSales, locationName);
+
+
+  ShamanCookieStores.renderAll();
+}
+var form = document.querySelector('form');
+form.addEventListener('submit', handleSubmit);
